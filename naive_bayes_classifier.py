fef56d8008d461be3e1e73613e0f1b96e7e02388
Zahan Malkani <zahanm@gmail.com>
2011-05-22 08:14:17 -0700

    naive_bayes_classifier.py added
    Equivalent starter code for naive_bayes_classifier added

---
+++ naive_bayes_classifier.py
@@ -0,0 +1,55 @@
+
+from __future__ import print_function
+
+import sys
+
+from message_iterator import MessageIterator
+
+def binomial(mi):
+  pass
+
+def binomial_chi2(mi):
+  pass
+
+def multinomial(mi):
+  pass
+
+def twcnb(mi):
+  pass
+
+def output_probability(probabilities):
+  for i, prob in enumerate(probabilities):
+    if i == 0:
+      sys.stdout.write("{0:1.8g}".format(prob))
+    else:
+      sys.stdout.write("\t{0:1.8g}".format(prob))
+    sys.stdout.write("\n")
+
+
+MODES = {
+    'binomial': binomial,
+    'binomial-chi2': binomial_chi2,
+    'multinomial': multinomial,
+    'twcnb': twcnb
+    # Add others here if you want
+    }
+
+def main():
+  if not len(sys.argv) == 3:
+    print("Usage: python {0} <mode> <train>".format(__file__), file=sys.stderr)
+    sys.exit(-1)
+  mode = sys.argv[1]
+  train = sys.argv[2]
+
+  mi = MessageIterator(train)
+
+  try:
+    MODES[mode](mi)
+  except KeyError:
+    print("Unknown mode: {0}".format(mode),file=sys.stderr)
+    print("Accepted modes are: {0}".format(MODES.keys()), file=sys.stderr)
+    sys.exit(-1)
+
+if __name__ == '__main__':
+  main()
+
